8553b376c670a418bebd2999c14192c0bd9af9dd
koneviahmet <koneviahmet@gmail.com>
2020-10-09 15:54:46 +0300

    merhaba dünya

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # excel-cli
 excel dosyalarını text dosyasına dilediğimiz formatta aktarmaya yarar.
+
+## kurulum
+> sudo npm i -g
+
+## kullanımı
+> excel-cli a.xlsx <sutun Ayracı> <Sutun Numaraları>
+
+
+burada sutun numaraları karışık sıralamaya göre olabilir.
+
+## örnek kullanımı
+
+> excel-cli a.xlsx - 2 1 3
---
+++ app.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+const fs            = require('fs');
+const slash         = require('slash');
+const afs           = require('./afs.js');
+const readXlsxFile  = require('read-excel-file/node');
+
+
+/* komut satırının başlatıldığı dizini alalım */
+const cliDizin = process.env.PWD;
+
+/* bu uygulamanın yüklü olduğu dizin */
+let appDizin = __dirname;
+if(process.env.OS == "Windows_NT"){appDizin = slash(dir);}
+
+let txtName           = process.argv[2];
+let txtYol            = cliDizin + "/" + txtName;
+let sutunArasiIsaret  = process.argv[3];
+let secilenStunlar    = process.argv.splice(4,process.argv.length - 4);
+
+/* dosya okumalarını asenkron yapacağız */
+async function asenkronAkis(){
+
+  await readXlsxFile(txtName).then((rows) => {
+    let genelText = "";
+    rows.forEach((item, ix) => {
+
+      let altText = "";
+      secilenStunlar.forEach((itemSutun, i) => {
+
+        altText += secilenStunlar.length - 1 != i ? item[itemSutun - 1] + sutunArasiIsaret : item[itemSutun - 1];
+        //console.log("satır  " + itemSutun, item[itemSutun - 1]);
+      });
+
+      genelText += rows.length - 1 != ix ? altText + "\n" : altText;
+
+    });
+
+
+    console.log(genelText);
+
+  });
+
+
+}
+
+asenkronAkis();
