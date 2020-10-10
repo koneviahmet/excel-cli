fd78443c4250426514e179441f4f2407d37219f7
koneviahmet <koneviahmet@gmail.com>
2020-10-10 21:48:31 +0300

    parametreler eklendi

--- README.md
+++ README.md
@@ -5,11 +5,27 @@ excel dosyalarını text dosyasına dilediğimiz formatta aktarmaya yarar.
 > sudo npm i -g
 
 ## kullanımı
-> excel-cli a.xlsx <sutun Ayracı> <Sutun Numaraları>
+> excel-cli a.xlsx
 
+## parametreler
+> -a
 
-burada sutun numaraları karışık sıralamaya göre olabilir.
+-a parametresi ayraç anlamına gelir. varsayılan olarak , ayracını kullanır
 
-## örnek kullanımı
+> -a #
 
-> excel-cli a.xlsx - 2 1 3
+şeklinde paramatre girersek , ayracını # ile değiştirir.
+
+>-s
+
+-s parametresi seçilen sutunları gösterir.
+
+> -s 3 2 1
+
+sutunları 3 2 1 şeklinde sıralamış oldu örnek kullanım aşağıdaki gibi olabilir.
+
+> -g 4
+
+-g grupndandırma için kullanılacak 4. sutunu guplandır demek
+
+> excel-cli a.xlsx -a , -s 3 2 1
--- app.js
+++ app.js
@@ -14,13 +14,58 @@ if(process.env.OS == "Windows_NT"){appDizin = slash(dir);}
 
 let txtName           = process.argv[2];
 let txtYol            = cliDizin + "/" + txtName;
-let sutunArasiIsaret  = process.argv[3];
-let secilenStunlar    = process.argv.splice(4,process.argv.length - 4);
+
+/* varsayılar olarak , */
+let sutunArasiIsaret  = ",";
+let secilenStunlar    = [];
+let groupBy           = 0;
+let groupByArr        = [];
+
+/* girilen değerlerden ilk üçünü silelim */
+process.argv.splice(0,3);
+
+
 
 /* dosya okumalarını asenkron yapacağız */
 async function asenkronAkis(){
 
   let genelText = "";
+
+  let tamKomut    = process.argv.join('*');
+  let altKomutlar = tamKomut.split("-").filter(item => item.length > 0);
+  for await (let komut of altKomutlar) {
+    let komutArr  = komut.split('*');
+
+
+    /* komutlardan gelen ayarları burada düzene sokalım */
+    if (komutArr[0] == "a") {
+      /* ayraç belrlemek istiyor demektir. */
+
+      sutunArasiIsaret = komutArr[1];
+    }else if (komutArr[0] == "s") {
+      /* sutun aralıklarını belirlemek istiyor demektir. */
+
+      /* komutun ilk değerini silelim */
+      komutArr.splice(0,1);
+
+      secilenStunlar = [...komutArr.filter(item => item.length > 0)];
+    }else if (komutArr[0] == "g") {
+      /* gruplama yapmak istiyor demektir.*/
+      groupBy = komutArr[1];
+
+
+    }
+
+
+  }
+
+
+  console.log("exelName", txtName);
+  console.log("sutun arası işaret", sutunArasiIsaret);
+  console.log("seçilen sutunlar", secilenStunlar);
+
+
+
   await readXlsxFile(txtName).then((rows) => {
 
     rows.forEach((item, ix) => {
@@ -28,18 +73,40 @@ async function asenkronAkis(){
       let altText = "";
       secilenStunlar.forEach((itemSutun, i) => {
 
-        altText += secilenStunlar.length - 1 != i ? item[itemSutun - 1] + sutunArasiIsaret : item[itemSutun - 1];
+          altText += secilenStunlar.length - 1 != i ? item[itemSutun - 1] + sutunArasiIsaret : item[itemSutun - 1];
+
         //console.log("satır  " + itemSutun, item[itemSutun - 1]);
       });
 
-      genelText += rows.length - 1 != ix ? altText + "\n" : altText;
+
+
+      if (groupBy) {
+        let groupByText = item[groupBy - 1].trim();
+
+        if (groupByArr.indexOf(groupByText) == -1) {
+          /* daha önce eklenmemiş ise ekleyelim */
+
+          genelText += rows.length - 1 != ix ? altText + "\n" : altText;
+          groupByArr.push(groupByText);
+        }
+      }else{
+
+        /* groupBy aktif değil ekle gitsin*/
+        genelText += rows.length - 1 != ix ? altText + "\n" : altText;
+
+      }
+
 
     });
 
+    /*
+    console.log(groupByArr);
+    console.log("adet", groupByArr.length);
+    */
 
     //console.log(genelText);
-
   });
+
 
 
   /* önemli not: her defasında altına ekleyerek gidiyor. */
